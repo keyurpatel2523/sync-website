c30f12742c24bfef23c13661ea560ae3c99fae86
Keyur Patel <110649559+keyurpatel2523@users.noreply.github.com>
2024-12-25 18:47:00 +0000

    feat: update header and footer

--- src/components/Contact/ContactInformation.tsx
+++ src/components/Contact/ContactInformation.tsx
@@ -15,28 +15,31 @@ const ContactInformation = () => {
       <p className="mb-11 border-b border-body-color border-opacity-25 pb-11 text-base leading-relaxed text-body-color dark:border-white dark:border-opacity-25">
         {t("contact_us_information")}
       </p>
-      <div>
-        <input
-          type="text"
-          name="name"
-          placeholder="Enter your name"
-          className="border-stroke mb-4 w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Enter your email"
-          className="border-stroke mb-4 w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
-        />
-        <input
-          type="submit"
-          value="Subscribe"
-          className="mb-5 flex w-full cursor-pointer items-center justify-center rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark"
-        />
-        <p className="text-center text-base leading-relaxed text-body-color dark:text-body-color-dark">
-          No spam guaranteed, So please don’t send any spam mail.
+      <div className="max-w-2xl mx-auto bg-white rounded-lg">
+        <p className="text-gray-700 mb-2">
+          📍 <span className="font-semibold">Address:</span> 364, Brigstock Road
+        </p>
+        <p className="text-gray-700 mb-2">
+          📞 <span className="font-semibold">Phone:</span> +44 7765104623
+        </p>
+        <p className="text-gray-700 mb-4">
+          📧 <span className="font-semibold">Email:</span>{" "}
+          <a
+            href={`mailto:keyur2523@gmail.com`}
+            className="text-blue-500 hover:underline"
+          >
+            keyur2523@gmail.com
+          </a>
+        </p>
+
+        <h3 className="text-xl font-semibold text-gray-800 mb-2">
+          Business Hours
+        </h3>
+        <p className="text-gray-700">
+          Monday to Friday: 09:00 AM - 06:00 PM
         </p>
       </div>
+      
 
       <div>
         <span className="absolute left-2 top-7">

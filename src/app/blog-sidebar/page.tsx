a30fb372a103d9c6858934e5cb11d4789521890b
Keyur Patel <110649559+keyurpatel2523@users.noreply.github.com>
2024-12-25 17:23:12 +0000

    feat(home): update our services and add other information into en.json

--- src/app/blog-sidebar/page.tsx
+++ src/app/blog-sidebar/page.tsx
@@ -1,7 +1,7 @@
 import RelatedPost from "@/components/Blog/RelatedPost";
 import SharePost from "@/components/Blog/SharePost";
 import TagButton from "@/components/Blog/TagButton";
-import NewsLatterBox from "@/components/Contact/NewsLatterBox";
+import ContactInformation from "@/components/Contact/ContactInformation";
 import Image from "next/image";
 
 import { Metadata } from "next";
@@ -459,7 +459,7 @@ const BlogSidebarPage = () => {
                 </div>
               </div>
 
-              <NewsLatterBox />
+              <ContactInformation />
             </div>
           </div>
         </div>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,6 +2,7 @@ import Breadcrumb from "@/components/Common/Breadcrumb";
 import Contact from "@/components/Contact";
 
 import { Metadata } from "next";
+import { useTranslations } from "next-intl";
 
 export const metadata: Metadata = {
   title: "Contact Page | Free Next.js Template for Startup and SaaS",
@@ -10,11 +11,13 @@ export const metadata: Metadata = {
 };
 
 const ContactPage = () => {
+  const t = useTranslations("contact_us");
+
   return (
     <>
       <Breadcrumb
-        pageName="Contact Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        pageName="Contact Us"
+        description={t("contact_us_description")}
       />
 
       <Contact />
